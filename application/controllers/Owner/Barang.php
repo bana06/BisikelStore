80d2842e8659f0a62138f5f906fe0c916188b206
bana <sabana.prilude@gmail.com>
2020-02-02 22:12:15 +0700

    ketinggalan

--- application/controllers/Owner/Barang.php
+++ application/controllers/Owner/Barang.php
@@ -80,11 +80,25 @@ class Barang extends CI_Controller {
 	    $config['max_height']           = 0;
 	    $this->load->library('upload', $config);
 	    if (!$this->upload->do_upload('photo_brg')){
-	            $error = array('error' => $this->upload->display_errors());
-	            $this->load->view('Owner/Barang/add_index', $error);
+	        $data = array(
+				'nama_brg'        => $this->input->post('nama_brg'),
+				'harga_brg'       => $this->input->post('harga_brg'),
+				'stok'            => $this->input->post('stok'),
+				'id_brand'        => $this->input->post('id_brand'),
+				'id_kategori_brg' => $this->input->post('id_kategori_brg'),
+				'deskripsi'       => $this->input->post('deskripsi'),
+				'id_status'       => 1,
+	            );
+	        $query = $this->all->menambah($this->table, $data);
+	        if($query){
+	            echo 'berhasil di upload';
+	            redirect(site_url('Owner/Barang'));
+	        }else{
+	            echo 'gagal upload';
+	        }
 	    }else{
 	        $_data = array('upload_data' => $this->upload->data());
-	         $data = array(
+	        $data = array(
 				'nama_brg'        => $this->input->post('nama_brg'),
 				'harga_brg'       => $this->input->post('harga_brg'),
 				'stok'            => $this->input->post('stok'),
@@ -130,7 +144,7 @@ class Barang extends CI_Controller {
 	    $config['overwrite']           = TRUE;
 	    $this->load->library('upload', $config);
 	    if (!$this->upload->do_upload('photo_brg')){
-	            $data = array(
+            $data = array(
 				'nama_brg'        => $this->input->post('nama_brg'),
 				'harga_brg'       => $this->input->post('harga_brg'),
 				'stok'            => $this->input->post('stok'),
@@ -138,7 +152,7 @@ class Barang extends CI_Controller {
 				'id_kategori_brg' => $this->input->post('id_kategori_brg'),
 				'deskripsi'       => $this->input->post('deskripsi'),
 				'id_status'       => 1,
-	            );
+            );
 	        $query = $this->all->update($this->table, ['id_brg'=>$id_brg], $data);
 	        if($query){
 	            echo 'berhasil di upload';
@@ -173,7 +187,7 @@ class Barang extends CI_Controller {
 	    //hapus gambar
 	    $get_data = $this->all->mengambil($this->table, ['id_brg'=>$id_brg])->row();
 	    $query = $this->all->delete($this->table, ['id_brg'=>$id_brg]);
-	    
+
 	    if ($query) {
 	    	unlink('assets/img/barang/'.$get_data->photo_brg);
 	    }
